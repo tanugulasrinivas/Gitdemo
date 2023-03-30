d049b5b109e71111e1d2347a8abcab48fa7e735e
tanugulasrinivas <tanugulsrinivas204@gmail.com>
2023-03-30 22:19:39 +0530

    this is asian time guy

--- src/demo/demo_1.java
+++ src/demo/demo_1.java
@@ -7,6 +7,8 @@ public class demo_1 extends Apputils {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		driver.get("http://facebook.com");
+		System.out.println("yes");
+		System.out.println("No");
 	}
 
 }
